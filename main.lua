003bfa0b022437288c5e1384bfa34092e81addcb
Rionte <66639993+Rionte@users.noreply.github.com>
2021-09-28 15:13:08 -0400

    changes to main

--- main.lua
+++ main.lua
@@ -2,28 +2,32 @@ suit = require 'lib.suit'
 SSM = require 'lib.ssm'
 
 function love.load()
+    width, height = love.window.getMode()
+    love.window.setMode(500, 700)
+    love.window.setTitle("Space Dodger")
+
     bg = love.graphics.newImage("images/bg.png")
     x = 0
     y = -5
     y2 = -5
-    width, height = love.window.getMode()
-    love.window.setMode(500, 700)
 end
 
 function love.update(dt)
-    y = y + 10
-    y2 = y2 + 10
+    -- Scroll Background --
+    y = y + 5
+    y2 = y2 + 5
 
-    if y == -700 then
+    if y >= 100 then
         y = 0
     end
 
-    if y2 == -700 then
+    if y2 >= 100 then
         y2 = 0
     end
 end
 
 function love.draw()
-    love.graphics.draw(bg, x, y-505)
+    -- Draw Background --
+    love.graphics.draw(bg, x, y-5)
     love.graphics.draw(bg, x, y2-505)
 end
